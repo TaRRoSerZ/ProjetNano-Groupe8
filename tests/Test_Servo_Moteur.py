870ac0b4a62aa3892c9a20cf542c2fc7c3d2e179
Deadzi06 <dowson.soupart1810@gmail.com>
2025-04-08 16:19:00 +0200

    feat : implémentation des tests des servo-moteurs

---
+++ tests/Test_Servo_Moteur.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import patch
+from classes.Servo_Moteur import Servo_Moteur
+
+
+class Test_Servo_Moteur(unittest.TestCase):
+
+    @patch('classes.Servo_Moteur.AngularServo')
+    def test_angle_bon(self, AngularServo):
+        mock_servo = AngularServo.return_value
+        mock_servo.angle = 0
+        servo = Servo_Moteur("servo", 4)
+        result = servo.regler_angle(0)
+        exptected = 0
+        self.assertEqual(result, exptected)
+
+    @patch('classes.Servo_Moteur.AngularServo')
+    def test_angle_hors_valeur_acceptable(self, AngularServo):
+        mock_servo = AngularServo.return_value
+        mock_servo.angle = -100
+        servo = Servo_Moteur("capteur", 4)
+
+        self.assertRaises(ValueError, servo.regler_angle(), 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
