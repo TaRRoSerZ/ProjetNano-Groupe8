c76845a4fc077d87a96681f7e6350e2e7fb9a174
TaRRoS-Dev <kyllianevrard1623@gmail.com>
2025-04-08 14:46:56 +0200

    feat : ajout du test du capteur ultrasons

--- tests/Test_CapteurUltrasons.py
+++ tests/Test_CapteurUltrasons.py
@@ -4,6 +4,19 @@ from classes.Capteur_Ultrasons import Capteur_Ultrasons
 
 
 class Test_CapteurUltrasons(unittest.TestCase):
+    """
+       Classe de tests unitaires pour la classe `Capteur_Ultrasons`.
+
+       Ces tests valident le comportement de la méthode `lire_donnee()` pour
+       s'assurer que le capteur ultrasons fonctionne correctement dans les
+       cas suivants :
+       - Lorsque la distance mesurée est trop petite.
+       - Lorsque la distance mesurée est normale.
+
+       Méthodes de test :
+           test_si_distance_trop_petite() : Teste si `lire_donnee()` retourne `None` pour une distance trop petite.
+           test_si_distance_normale() : Teste si `lire_donnee()` retourne la distance correcte pour une mesure normale.
+       """
 
     @patch('classes.Capteur_Ultrasons.DistanceSensor')
     def test_si_distance_trop_petite(self, MockDistanceSensor):
