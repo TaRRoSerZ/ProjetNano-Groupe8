d31c40b4e3ed75db5968f2a620ad2cb013cb1543
Kleos Sphère <kyllianevrard1622@gmail.com>
2025-04-11 16:06:10 +0200

    feat : ajout de docstrings à Test_Capteur_RGB

--- tests/Test_Capteur_RGB.py
+++ tests/Test_Capteur_RGB.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 from unittest.mock import MagicMock, PropertyMock
 
-# Création de faux modules avant l'import du code à tester
+
 sys.modules['board'] = MagicMock()
 sys.modules['board'].SCL = MagicMock()
 sys.modules['board'].SDA = MagicMock()
@@ -13,8 +13,12 @@ sys.modules['adafruit_tcs34725'] = MagicMock()
 from classes.Capteur_RGB import Capteur_RGB  
 
 class Test_Capteur_RGB(unittest.TestCase):
+    """
+    Classe qui permet de tester le code du capteur RGB que l'on utilise pour détecter la lumière verte et démarrer la voiture lors du
+    passage du feu au vert. 
+    """
+    
     def setUp(self):
-        # Configuration du capteur mocké
         self.mock_tcs = sys.modules['adafruit_tcs34725'].TCS34725.return_value
         type(self.mock_tcs).color_rgb_bytes = PropertyMock(return_value=(100, 150, 200))
 
