63374dc39c92b9b6b6253e25fbbc1fa17f8eda5f
Kleos Sphère <kyllianevrard1622@gmail.com>
2025-04-11 12:21:54 +0200

    fix : derniers fixbug voiture

--- classes/Voiture.py
+++ classes/Voiture.py
@@ -6,7 +6,6 @@ import time
 from threading import Thread, Lock
 
 
-
 class Voiture:
     def __init__(self, nom):
         self._nom = nom
@@ -18,20 +17,16 @@ class Voiture:
         self._capteur_ligne = Capteur_IR("Capteur IR", 20)
 
         # État partagé pour contrôler la voiture
-        self._etat_voiture = "AVANCER"  # L'état peut être "AVANCER", "EVITER_OBSTACLE", etc.
+        self._etat_voiture = True
         self._lock = Lock()
-
 
     @property
     def etat_voiture(self):
         return self._etat_voiture
 
-
     @property
     def lock(self):
         return self._lock
-
-
 
     @property
     def nom(self):
@@ -73,159 +68,184 @@ class Voiture:
     def etat_voiture(self, etat_voiture):
         self._etat_voiture = etat_voiture
 
+    def suivre_mur(self, direction="droite"):
 
-    def faire_demi_tour(self):
-        self._servo.centrer()
-        self._moteur.avancer(60)
-        time.sleep(2)
-        self._servo.tourner_droite()
-        time.sleep(2)
-        self._servo.centrer()
-        time.sleep(2)
-        self._moteur.stop()
-        self._servo.desactiver_pwm()
+        if direction == "droite":
+            capteur = self._capteur_droit
+            while self._etat_voiture:
+                print("Suivre le mur...")
+                distance = capteur.lire_donnee() * 100
+                if distance <= 20 or distance >= 100:
+                    self._servo.tourner_gauche()
+                    time.sleep(0.3)
+                elif distance >= 30:
+                    self._servo.tourner_droite()
+                    time.sleep(0.3)
+                else:
+                    self._servo.centrer()
+                time.sleep(0.2)
+        else:
+            pass
+
+    def detecter_collision(self):
+        while self._etat_voiture:
+            self._moteur.avancer(30)  # Démarrer la voiture en ligne droite
+            self._servo.centrer()  # Centrer le servo
+            distance_avant = self._capteur_avant.lire_donnee() * 100
+            distance_gauche = self._capteur_gauche.lire_donnee() * 100
+            distance_droite = self._capteur_droit.lire_donnee() * 100
+
+            if distance_avant <= 20:  # Si un obstacle est détecté à l'avant
+                print("Obstacle détecté à l'avant, ralentissement...")
+                self._moteur.stop()  # stop
+                time.sleep(0.5)
+                self._servo.tourner_gauche()
+                self._moteur.reculer(-30)
+                time.sleep(1)
+                self._moteur.stop()
+                time.sleep(0.5)
+                self._servo.centrer()
+                self._moteur.avancer(30)  # Avancer après avoir évité l'obstacle
+                if distance_gauche > distance_droite:  # Plus d'espace à gauche
+                    print("Esquive vers la gauche...")
+                    self._servo.tourner_gauche()
+                    self._moteur.avancer(30)
+                    time.sleep(0.15)
+                    self._servo.centrer()
+                elif distance_droite > distance_gauche:  # Plus d'espace à droite
+                    print("Esquive vers la droite...")
+                    self._servo.tourner_droite()
+                    self._moteur.avancer(30)
+                    time.sleep(0.15)
+                    self._servo.centrer()
+
+
+            elif distance_gauche > distance_droite:  # Plus d'espace à gauche
+                print("Esquive vers la gauche...")
+                self._servo.tourner_gauche()
+                time.sleep(0.15)
+                self._servo.centrer()
+
+            elif distance_droite > distance_gauche:  # Plus d'espace à droite
+                print("Esquive vers la droite...")
+                self._servo.tourner_droite()
+                time.sleep(0.15)
+                self._servo.centrer()
+
+            elif distance_avant <= 20 and distance_gauche <= 11:
+                self._moteur.stop()
+                self._servo.centrer()
+                self._moteur.reculer(-30)  # Reculer de 20 cm
+                time.sleep(1)
+                self._moteur.stop()
+                time.sleep(1)
+                self._servo.tourner_droite()
+                self._moteur.avancer(30)
+                time.sleep(0.3)
+                self._moteur.stop()
+                self._servo.tourner_gauche()
+                time.sleep(0.1)
+                self._servo.centrer()
+                self._moteur.avancer(30)
+
+
+            elif distance_avant <= 20 and distance_droite <= 11:
+                self._moteur.stop()
+                self._servo.centrer()
+                self._moteur.reculer(-30)  # Reculer de 20 cm
+                time.sleep(1)
+                self._moteur.stop()
+                time.sleep(1)
+                self._servo.tourner_gauche()
+                self._moteur.avancer(30)
+                time.sleep(0.3)
+                self._moteur.stop()
+                self._servo.tourner_droite()
+                time.sleep(0.1)
+                self._servo.centrer()
+                self._moteur.avancer(30)
+        time.sleep(0.3)
+
+        # else:
+        #     print("Obstacle trop proche, arrêt...")
+        #     self._moteur.stop()  # Arrêter si aucun espace disponible
+        #     break
+        # else:
+        #     print("Aucun obstacle détecté, avancer...")
+        #     self._servo.centrer()
+        #     self._moteur.avancer(30)  # Vitesse normale
+
+    # def eviter_obstacles(self):
+    #     if self.detecter_collision_avant() and self.detecter_collision_droite():
+    #         self._moteur.stop()
+    #         time.sleep(1)
+    #         self._moteur.reculer(-20)
+    #         time.sleep(1)
+    #         self._moteur.stop()
+    #         time.sleep(1)
+    #         self._servo.tourner_gauche()
+    #         self._moteur.avancer(20)
+    #         time.sleep(1)
+    #         self._moteur.stop()
+    #         self._servo.tourner_droite()
+    #         time.sleep(0.06)
+    #         self._servo.centrer()
+
+    #     elif self.detecter_collision_avant() and self.detecter_collision_gauche():
+    #         self._moteur.stop()
+    #         time.sleep(1)
+    #         self._moteur.reculer(-20)
+    #         time.sleep(1)
+    #         self._moteur.stop()
+    #         time.sleep(1)
+    #         self._servo.tourner_droite()
+    #         self._moteur.avancer(20)
+    #         time.sleep(1)
+    #         self._moteur.stop()
+    #         self._servo.tourner_gauche()
+    #         time.sleep(0.06)
+    #         self._servo.centrer()
 
     def aller_tout_droit(self):
         self._servo.centrer()
-        self._moteur.avancer(100)
-        time.sleep(5)
-        self._moteur.stop()
-        self._servo.desactiver_pwm()
-
-    def detecter_collision_avant(self):
-
-        while True:
-            distance = self._capteur_avant.lire_donnee() * 100  # Conversion de la distance en cm
-            print(f"Distance détectée : {distance} cm")
-            if distance <= 10:
-                self.moteur.avancer(10)
-                return False
-            time.sleep(0.3)
-
-    def detecter_collision_gauche(self):
-
-        while True:
-            distance = self.capteur_gauche.lire_donnee() * 100
-            if self.detecter_collision_avant() == False:
-                return
-            elif distance <= 10:
-                self._servo.tourner_droite()
-                return None
-            time.sleep(0.3)
-
-    def detecter_collision_droite(self):
-
-        while True:
-            distance = self.capteur_droit.lire_donnee() * 100
-            if distance <= 10:
-                self._servo.tourner_gauche()
-                return None
-            time.sleep(0.3)
-
-    def demi_tour_en_T(self):
-        self._servo.centrer()
-        self._moteur.avancer(60)
-        time.sleep(0.3)
-        self._servo.tourner_gauche()
-        time.sleep(1)
-        self._moteur.stop()
-        self._servo.centrer()
-        time.sleep(0.1)
-        self._servo.tourner_droite()
-        time.sleep(1)
-        self._moteur.reculer()
-        time.sleep(1)
-        self._moteur.stop()
-        self._servo.centrer()
-        self._moteur.avancer()
-        time.sleep(1)
-        self._moteur.stop()
-        self._servo.desactiver_pwm()
-
-    def grand_8(self):
-        self._servo.centrer()
-        self._moteur.avancer(60)
-        time.sleep(0.5)
-        self._servo.tourner_droite()
-        time.sleep(4.5)
-        self._servo.centrer()
-        time.sleep(0.3)
-        self._servo.tourner_gauche()
-        time.sleep(3.2)
-        self._moteur.stop()
-        self._servo.centrer()
-        self._servo.desactiver_pwm()
+        self._moteur.avancer(20)
 
     def arreter_voiture(self):
         self._moteur.stop()
+        self._servo.desactiver_pwm()
+        self._etat_voiture = False  # Mettre à jour l'état de la voiture
 
-    def detecter_ligne_arrivee(self):
-        time.sleep(5)
-        while True:
-            if self._capteur_ligne.lire_donnee():
-                ligne_noire = self._capteur_avant.lire_donnee()
+    def compteur_tour(self, nb_tour):
+        compte = nb_tour
+        print(f"Nombre de tours restants : {compte}")
+        while compte > 0:
+            if not self._capteur_ligne.lire_donnee():
+                print(f"Tour terminé. Tours restants : {compte - 1}")
+                compte -= 1
+                time.sleep(2)  # Pause pour éviter de détecter la même ligne plusieurs fois
+        self.arreter_voiture()
 
-                if ligne_noire:
-                    print("Ligne noire détectée")
-                    self.arreter_voiture()
-                    break
-        time.sleep(0.1)
+    def course(self, nb_tour):
 
-    def eviter_obstacles(self):
-        if self.detecter_collision_avant() and self.detecter_collision_droite():
-            self._moteur.stop()
-            time.sleep(0.1)
-            self._moteur.reculer()
-            time.sleep(0.1)
-            self._moteur.stop()
-            time.sleep(0.1)
-            self._servo.tourner_gauche()
-            self._moteur.avancer()
-            time.sleep(0.1)
-            self._moteur.stop()
-            self._servo.tourner_droite()
-            time.sleep(0.06)
-            self._servo.centrer()
+        # Thread pour détecter les collisions et éviter les obstacles
+        thread_collision = Thread(target=self.detecter_collision)
 
-        elif self.detecter_collision_avant() and self.detecter_collision_gauche():
-            self._moteur.stop()
-            time.sleep(0.1)
-            self._moteur.reculer()
-            time.sleep(0.1)
-            self._moteur.stop()
-            time.sleep(0.1)
-            self._servo.tourner_droite()
-            self._moteur.avancer()
-            time.sleep(0.1)
-            self._moteur.stop()
-            self._servo.tourner_gauche()
-            time.sleep(0.06)
-            self._servo.centrer()
+        # Thread pour compter les tours
+        thread_tours = Thread(target=self.compteur_tour, args=(nb_tour,))
+
+        thread_mur = Thread(target=self.suivre_mur, args=("droite",))
+
+        # Démarrage des threads
+        thread_collision.start()
+        thread_tours.start()
+        thread_mur.start()
+
+        # Attente de la fin des threads
+        thread_collision.join()
+        thread_tours.join()
+        thread_mur.join()
 
 
-
-
-    def demarrer(self):
-        thread_mouvement = Thread(target=self.aller_tout_droit)
-        thread_capteur_ultrasons_avant = Thread(target=self.detecter_collision_avant)
-        thread_capteur_ultrasons_gauche = Thread(target=self.detecter_collision_gauche)
-        thread_capteur_ultrasons_droite = Thread(target=self.detecter_collision_droite)
-        thread_ligne = Thread(target=self.detecter_ligne_arrivee)
-
-
-        thread_mouvement.start()
-        thread_capteur_ultrasons_avant.start()
-        thread_capteur_ultrasons_gauche.start()
-        thread_capteur_ultrasons_droite.start()
-        thread_ligne.start()
-
-        thread_mouvement.join()
-        thread_capteur_ultrasons_avant.join()
-        thread_capteur_ultrasons_gauche.join()
-        thread_capteur_ultrasons_droite.join()
-        thread_ligne.join()
-
-
+# Création de l'objet voiture et démarrage de la course
 voiture = Voiture("Vroum-Mobile")
-voiture.demarrer()
-
+voiture.course(3)
