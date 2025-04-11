cc711681e65fea9163e6fa8baf6ebf8672e20cf8
Kleos Sphère <kyllianevrard1622@gmail.com>
2025-04-11 15:56:03 +0200

    feat : ajout docstrings à la classe Loggeur

--- classes/Loggeur.py
+++ classes/Loggeur.py
@@ -1,6 +1,9 @@
 import logging
 
 def setupLoggeur():
+    """
+    Méthode qui permet de mettre en place le fichier logs.txt, écrire dedans si il existe et le créer s'il n'est pas encore dans l'arborescance.
+    """
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime) s - %(levelname)s - %(message)s',
@@ -10,6 +13,12 @@ def setupLoggeur():
     )
 
 def lireLogs(limite):
+    """
+    Méthode qui permet de lire les logs et de les afficher dans la console. 
+
+    args :
+        - limite : permet de définir le nombre de ligne qui va être affiché dans la console (en partant du log le plus récent)
+    """
     if type(limite) != int:
         limite = 20
 
